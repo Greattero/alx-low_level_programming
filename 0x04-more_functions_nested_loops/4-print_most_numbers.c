a817f450d08feb18727b8d592a5bb890169c0bf1
Foster Ametepey <fostericetwo@gmail.com>
2023-02-23 12:33:29 -0800

    Print 0-9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,17 @@
-/*
- * File: 3-print_numbers.c
- * Auth: Ametepey Foster
- */
-
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0-9.
+ * print_most_numbers - print num except 2 and 4
+ *
+ * return void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-	int num;
+	char c;
 
-	for (num = 0; num <= 9; num++)
-		_putchar((num % 10) + '0');
-
+	for (c='0',c<='9',c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
+	}
 	_putchar('\n');
-}
-
